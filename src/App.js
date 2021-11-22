1673859f2d9816f5c621830d4c8ddecffe6e9bc6
Prem Acharya <prem.acharya@hotmail.com>
2021-11-22 14:18:19 +1100

    login and register form added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Login from "./pages/login/Login";
 import "./App.scss";
+import Register from "./pages/register/register";
 
 function App() {
 	return (
-		<div className="">
-			hwollowowowoow <i class="fas fa-user"></i>
-		</div>
+		<Router>
+			<Routes>
+				<Route path="/" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+			</Routes>
+		</Router>
 	);
 }
 
