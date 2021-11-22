cfabb9ef610c96500de221372068716b0c1779ad
Prem Acharya <prem.acharya@hotmail.com>
2021-11-22 12:47:56 +1100

    added react router dom and fontawesome

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.scss";
 
 function App() {
-	return <div className="">hwollowowowoow</div>;
+	return (
+		<div className="">
+			hwollowowowoow <i class="fas fa-user"></i>
+		</div>
+	);
 }
 
 export default App;
